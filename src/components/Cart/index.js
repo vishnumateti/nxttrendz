b732a4d95ce342bb501da01bf21d204df98cbbe5
vishnu mateti <vishnukiran108@gmail.com>
2023-07-21 09:10:48 +0000

    Nxt Trendz

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import Header from '../Header'
+
+const Cart = () => (
+  <div>
+    <Header />
+    <img
+      alt="cart"
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png "
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import Header from '../Header'
+
+const Products = () => (
+  <div>
+    <Header />
+    <img
+      alt="products"
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png "
+    />
+  </div>
+)
+
+export default Products
